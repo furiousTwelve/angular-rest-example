b5aedf12e79e63e21ae51d10ef33220599b64550
David di Marcantonio <david@code4mac.com>
2016-12-06 11:41:40 +0100

    add promise to post from controller
    post user to service which connects to web server

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -8,10 +8,20 @@
  * Controller of the angresexaApp
  */
 angular.module('angresexaApp')
-  .controller('UserCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+  .controller('UserCtrl',['$scope', function ($scope) {
+      /* -- send login request to server -- */
+      $scope.createUser = function() {
+        connectToRest.postUser().save($scope.inputs).$promise.then(
+          function success(response) {
+            if(response.error != 0) {
+              $scope.hasErrors = true;
+              $scope.message = response.message;
+              return;
+            }
+            console.log('votre utilisateur a été crée');
+            console.dir(response);                      //response contient un json
+            console.log(response.status);
+          }
+        );
+      };
+  }]);
