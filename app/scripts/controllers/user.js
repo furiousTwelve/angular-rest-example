58703e37685fe9b9b6c1bbdc478759099fa93acd
David <david@code4mac.com>
2016-12-18 22:57:44 +0100

    add typical form with file and inputs (need to be deguged)

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -10,11 +10,50 @@
 angular.module('angresexaApp')
   .controller('UserCtrl',['$scope', function ($scope) 
    {
-     $scope.email = "Sam@Soungue.com";
-     $scope.password = "passw";
+
+      /**
+       * clear error message 
+       * */
+      $scope.clearError = function() { $scope.hasErrors = false; };
+
+      /**
+       * upload files in a different thread
+       */
+      $scope.uploadFiles = function (files) {
+        $scope.files = files;
+        if (files && files.length) {
+          $scope.photo = connectToRest.uploadAlbumFiles(files).then(function (response) {
+            $timeout(function () {
+              $scope.inputs.album = response.data.files;
+            });
+          });
+        }
+      };
+
+      /**
+       * upload a file in a different thread
+       */
+      $scope.uploadCouverture = function(file, errFiles) {
+        $scope.fc = file;
+        $scope.errFile = errFiles && errFiles[0];
+        if (file) {
+          connectToRest.uploadFile(file).then(function (response) {
+            $timeout(function () {
+              $scope.inputs.couverture = response.data.file;
+            });
+          });
+        }
+      };
+
+    //  $scope.email = "Sam@Soungue.com"; not still usefull
+    //  $scope.password = "passw";
       /* -- send login request to server -- */
+      /**
+       * called from html form
+       * Create a new user from html form
+       */
       $scope.createUser = function() {
-        connectToRest.postUser().save($scope).$promise.then(           //in a form I would pass $scope.inputs rather than $scope
+        connectToRest.postUser().save($scope.inputs).$promise.then(           
           function success(response) {
             if(response.error != 0) {
               $scope.hasErrors = true;
--- app/scripts/services/connecttorest.js
+++ app/scripts/services/connecttorest.js
@@ -6,11 +6,12 @@
  * @description
  * # connectToRest
  * Service in the angresexaApp.
+ * TODO : take a look at <script src="bower_components/ng-file-upload/ng-file-upload.js"></script> to implement UPLOAD
  */
 angular.module('angresexaApp')
   .constant('baseURL', 'http://rest.example.com')
-  .service('connectToRest',[ '$resource', 'baseURL',
-    function ($resource, baseURL) 
+  .service('connectToRest',[ '$resource', 'baseURL','Upload',
+    function ($resource, baseURL, Upload)
     {
       // #####   Example to GET an array of json 
       this.getUsers = function() 
@@ -43,6 +44,29 @@ angular.module('angresexaApp')
         {
           return $resource(baseURL+'/contact', null, { 'save': { method: 'POST' } });
         };
+
+
+      /**
+       * Send mutli files
+       */
+      this.uploadAlbumFiles = function (files) {
+        return Upload.upload({
+            url: baseURL+'/file/files',
+            data: {files: files}
+        });
+      };
+
+     /** 
+      * send unic file 
+      */ 
+    this.uploadSouvenirFile = function (file)
+    {
+      return Upload.upload({
+          url: baseURL+'/file/file',
+          data: {file: file}
+      });
+    };
+
 
 
    }]);
